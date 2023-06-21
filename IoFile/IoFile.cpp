f920001d0bdd9186d85038b4a51c9cd95cbee1dd
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-06-21 14:52:09 +0700

    loop akan berhenti jika kita memasukkan karakter q

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -18,5 +18,7 @@ int main() {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika kita memasukkan karakter q
+		if (baris == "q")break;
 	}
 }
