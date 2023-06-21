c0a0361e364b4c38d8cbedec039749b726561a86
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-06-21 15:33:01 +0700

    membuat file

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -16,5 +17,36 @@ int main() {
 
 	cout << ">= Menulis File, \'q\' untuk keluar" << endl;
 
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika kita memasukkan karakter q
+		if (baris == "q")break;
+		//menulis dan memasukkan nilai dari 'baris' kedalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
 
+	//membuka file mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan Membaca File " << endl;
+	//jika fie ada maka
+	if (infile.is_open()) {
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris)) {
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable To Open File";
+	return 0;
 }
